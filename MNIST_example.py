1409268bddb97e9fcb4f6e20fbbbfb96c33c3ca9
KatNykiel <katanykiel@gmail.com>
2022-05-27 13:52:44 -0400

    introduced ising plot function

---
+++ MNIST_example.py
@@ -0,0 +1,11 @@
+# MNIST_example.py
+# Work through a basic restricted Boltzmann machine with the MNIST handwriting database
+# Kat Nykiel
+
+# %% codecell
+def get_Ising_plot(array = [[1/2,-1/2],[-1/2,1/2]])
+    # Plots an array of spins
+    # input: an array (or list of arrays) corresponding to spins in an Ising model
+    # output: a visual representation of the array, either as an image or video
+
+    return True
